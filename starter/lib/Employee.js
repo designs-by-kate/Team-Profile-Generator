8fb966287a8c2aee2d01658ec2b33ba9df79ec2e
designs-by-kate <kate_diet@hotmail.com>
2024-02-23 11:37:44 +0000

    Created classes for each team member, I didn't test them yet

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,49 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.validateName(name);
+        this.validateId(id);
+        this.validateEmail(email);
+
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    validateName() {
+        if (typeof name !== 'string' || this.name.trim() == '') {
+            throw new Error('Name must be a non-empty string.');
+        }
+    }
+
+    validateId() {
+        if (typeof id !== 'number' || isNaN(id) || id <= 0) {
+            throw new Error('ID must be a positive number.');
+        }
+    }
+
+    validateEmail() {
+        // Basic email validation using a regular expression
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            throw new Error('Invalid email format.');
+        }
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+}
+
+module.exports = Employee;
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+        this.validateOfficeNumber();
+    }
+
+    validateOfficeNumber() {
+        if (typeof this.officeNumber !== 'number' || isNaN(this.officeNumber)) {
+            throw new Error('Office number must be a valid number.');
+        }
+    }
+
+    getRole() {
+        return 'Manager';
+    }
+}
+
+module.exports = Manager;
